d6e3795e5548c8eb37a0965c3957ff79ddf9ebdd
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-07-01 13:45:34 -0500

    Merge branch 'main' of https://github.com/Adam-McNeil/Oasis-Digital-Intern-Kanban-Board into level
    # Conflicts:
    #	Assets/Materials/Blue.mat
    #	Assets/Materials/Blue.mat.meta
    #	Assets/Scenes/Main.unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerController : NetworkBehaviour
+{
+    [Header("Adjustible Variables ")]
+    [SerializeField] private float speed;
+    [SerializeField] private float sensitivity;
+    private CharacterController characterController;
+    [Header("Child Refences")]
+    [SerializeField] private Camera myCamera;
+    private float xRotation = 0;
+    private float yRotation = 0;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+
+        if (isLocalPlayer)
+        {
+            myCamera.gameObject.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (isLocalPlayer)
+        {
+            DoMovement();
+            RotateCamera();
+        }
+    }
+
+    void DoMovement()
+    {
+        Vector3 motion = (transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical")).normalized * speed;
+        characterController.Move(motion);
+    }
+
+    void RotateCamera()
+    {
+        float xMouseChange = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+        float yMouseChange = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+
+        Debug.Log(xMouseChange);
+        Debug.Log(yMouseChange);
+
+        yRotation += xMouseChange;
+        xRotation -= yMouseChange;
+
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+        myCamera.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+    }
+}
