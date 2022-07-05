b05c0e5ffac3d18dc0ffb7e396c930dd471aa857
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-07-05 14:40:45 -0500

    Fixed Merge

--- Assets/Scripts/SpawnTickets.cs
+++ Assets/Scripts/SpawnTickets.cs
@@ -15,19 +15,21 @@ public class SpawnTickets : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         if(canInteract && Input.GetKeyDown("space")){
             Instantiate(ticketGameObject, spawnPosition, ticketGameObject.transform.rotation);
         }
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other) 
+    {
         //Debug.Log("Player Entered Area");
         canInteract = true;
     }
 
-    private void OnTriggerExit(Collider other) {
+    private void OnTriggerExit(Collider other) 
+    {
         //Debug.Log("Player Exited Area");
         canInteract = false;
     }
