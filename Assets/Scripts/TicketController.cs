ef48604f4ab1bff6e37f6e9743a20f09a21aa30a
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-07-06 10:46:20 -0500

    working network ticket movement

--- Assets/Scripts/TicketController.cs
+++ Assets/Scripts/TicketController.cs
@@ -7,20 +7,70 @@ using TMPro;
 public class TicketController : NetworkBehaviour
 {
     //static List<TicketInfo> ticketList = new List<TicketInfo>();
+    //private TicketInfo myTicketInfo;
+    private float speed = 1;
+    private float maxSpeed = 5;
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private TextMeshProUGUI textObject;
-    //private TicketInfo myTicketInfo;
+    private TicketTrigger ticketTriggerScript;
+    private Rigidbody body;
 
-    [SyncVar(hook = nameof(ChangeText))]
+    private float xInput;
+    private float zInput;
+    [SyncVar(hook = nameof (ChangeText))]
     private string textSyncVar;
+
+    private bool shouldDoMovement = true;
+
+    //[SyncVar(hook = nameof (ChangePosition))]
+    //private Vector3 positionSyncVar;
 
     private void Start()
     {
+        body = GetComponent<Rigidbody>();
+        ticketTriggerScript = GetComponentInChildren<TicketTrigger>();
         inputField.onValueChanged.AddListener(delegate { OnChangedInputField(); });
         //myTicketInfo.networkIdentity = GetComponent<NetworkIdentity>();
         //ticketList.Add(myTicketInfo);
     }
 
+    private void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        zInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (shouldDoMovement && ticketTriggerScript.beingEdited)
+        {
+            Vector3 force = new Vector3(xInput, 0, zInput).normalized * speed;
+            if (force.magnitude > 0)
+            {
+                AddForceCmd(force);
+            }
+           
+        }
+        Debug.Log(transform.position);
+    }
+
+    [Command(requiresAuthority = false)]
+    private void AddForceCmd(Vector3 force)
+    {
+        body.AddForce(force, ForceMode.Impulse);
+        if (body.velocity.magnitude > maxSpeed)
+        {
+            body.velocity = body.velocity.normalized * maxSpeed;
+        }
+    }
+    
+    public void SetShouldDoMovement(bool value)
+    {
+        shouldDoMovement = value;
+    }
+
+
+    #region SyncText
     private void OnChangedInputField()
     {
         ChangeTextCmd(inputField.text);
@@ -36,6 +86,7 @@ public class TicketController : NetworkBehaviour
     {
         textObject.text = newText;
     }
+    #endregion
 }
 
 /*
