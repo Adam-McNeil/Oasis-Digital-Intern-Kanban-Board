7571219974ea31e24f57be5b27ffebdf039a26e5
loganwyas <loganw9999@gmail.com>
2022-07-06 14:43:32 -0500

    Create ticket screen

--- Assets/Scripts/FirebaseUploader.cs
+++ Assets/Scripts/FirebaseUploader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using Firebase;
 using Firebase.Firestore;
@@ -29,43 +30,81 @@ public struct Ticket {
     [FirestoreProperty]
     public long LastUpdated { get; set; }
 
+    [FirestoreProperty]
+    public double X { get; set; }
+
+    [FirestoreProperty]
+    public double Y { get; set; }
+
+    [FirestoreProperty]
+    public double Z { get; set; }
+
 }
 
 public class FirebaseUploader : MonoBehaviour
 {
     private FirebaseApp app;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    public TMP_InputField titleInput;
+    public TMP_InputField descriptionInput;
+    public TMP_Dropdown statusInput;
+    public TMP_Dropdown assignedInput;
+
+    void Start() {
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
             var dependencyStatus = task.Result;
             if (dependencyStatus == Firebase.DependencyStatus.Available) {
                 app = Firebase.FirebaseApp.DefaultInstance;
             }
             else {
-                UnityEngine.Debug.LogError(System.String.Format("Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                Debug.LogError(System.String.Format("Could not resolve all Firebase dependencies: {0}", dependencyStatus));
             }
         });
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 
     public void createTicket() {
-        var date = DateTimeOffset.Now.ToUnixTimeSeconds();
-        var ticket = new Ticket {
-            Title = "Test",
-            Description = "Whatever",
-            Status = "On Hold",
-            TicketReporter = "Me",
-            AssignedUser = null,
-            DateCreated = date,
-            LastUpdated = date
-        };
+        createTicketReturnId();
+    }
+
+    public string createTicketReturnId() {
+        var ticket = createTicketTemplate();
+        ticket.Title = titleInput.text;
+        ticket.Description = descriptionInput.text;
+        ticket.Status = statusInput.options[statusInput.value].text;
+        ticket.AssignedUser = assignedInput.options[assignedInput.value].text;
         var firestore = FirebaseFirestore.DefaultInstance;
-        firestore.Collection("tickets").Document().SetAsync(ticket);
+        var document = firestore.Collection("tickets").Document();
+        document.SetAsync(ticket);
+        return document.Id;
+    }
+
+    // public void updateTicket(string id) {
+    //     var firestore = FirebaseFirestore.DefaultInstance;
+    //     var document = firestore.Collection("tickets").Document(id);
+    //     document.SetAsync(createTicketTemplate());
+    // }
+
+    private long getCurrentDate() {
+        return DateTimeOffset.Now.ToUnixTimeSeconds();
+    }
+
+    private Ticket createTicketTemplate(string title = "Test", string description = "Whatever", string status = "On Hold", string reporter = "Me", string assignedUser = null) {
+        var date = getCurrentDate();
+        return new Ticket {
+            Title = title,
+            Description = description,
+            Status = status,
+            TicketReporter = reporter,
+            AssignedUser = assignedUser,
+            DateCreated = date,
+            LastUpdated = date,
+            X = 0,
+            Y = 0,
+            Z = 0
+        };
     }
 }
