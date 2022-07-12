a688074aa35756aef98ee4d87b2474ad374d9249
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-07-12 15:18:53 -0500

    add column prefab

--- Assets/Scripts/AirTube.cs
+++ Assets/Scripts/AirTube.cs
@@ -1,10 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
 public class AirTube : MonoBehaviour
 {
-
     private List<GameObject> objectsOnConveyor = new List<GameObject>(); //Stores a list of gameobjects that are on the conveyor belt
     private GameObject movingObject;                                     //Objects that will be stored in the list to be moved  
     private Rigidbody movingObjectRB;
@@ -13,10 +13,9 @@ public class AirTube : MonoBehaviour
     public Vector3 direction;                                            //Direction where the object is pushed 
     public float speed;                                                  //Speed for the movement of the object 
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void Update()
@@ -40,7 +39,8 @@ public class AirTube : MonoBehaviour
     }
 
     private void OnTriggerStay(Collider other) {
-        if(isActive){
+        if(isActive && other.gameObject.CompareTag("Ticket"))
+        {
             movingObjectRB = other.gameObject.GetComponent<Rigidbody>();
             movingObjectRB.useGravity = false;
             movingObjectRB.constraints = RigidbodyConstraints.FreezeAll;
@@ -50,7 +50,7 @@ public class AirTube : MonoBehaviour
 
     private void OnTriggerExit(Collider other) 
     {
-        if(isActive){
+        if(isActive && other.gameObject.CompareTag("Ticket")){
             movingObjectRB.constraints = RigidbodyConstraints.None;
             movingObjectRB.useGravity = true; 
         }
---
+++ Assets/Scripts/StraightenTicket.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class StraightenTicket : NetworkBehaviour
+{
+    private Rigidbody ticketRigidbody;
+
+    private void Start()
+    {
+        if (isClientOnly)
+        {
+            Destroy(this.GetComponent<StraightenTicket>());
+        }
+        ticketRigidbody = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Straighten Ticket"))
+        {
+            Transform spawnTransform = other.GetComponentInChildren<Transform>();
+            this.gameObject.transform.position = spawnTransform.position;
+            this.gameObject.transform.rotation = spawnTransform.rotation;
+            this.gameObject.transform.Rotate(-90, 0, 0);
+            ticketRigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+}
