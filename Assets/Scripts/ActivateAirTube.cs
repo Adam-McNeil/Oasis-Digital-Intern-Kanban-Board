0b99df89e9c57ddec947f179a3f80411bddd2843
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-07-12 10:38:03 -0500

    Added Airtubes and Scripts

---
+++ Assets/Scripts/ActivateAirTube.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateAirTube : MonoBehaviour
+{
+
+    public GameObject airPad;
+    private AirTube airTubeScript;
+    private bool canInteract;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canInteract = false;
+        airTubeScript = airPad.GetComponent<AirTube>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canInteract && Input.GetMouseButtonDown(0) && !airTubeScript.isActive)
+        {
+            airTubeScript.isActive = true;
+        }
+
+        if(airTubeScript.isActive){
+            timer += Time.deltaTime;
+
+            if(timer > 2){
+                airTubeScript.isActive = false; 
+                timer = 0;
+            }
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other) 
+    {
+        canInteract = true;
+    }
+
+    private void OnTriggerExit(Collider other) 
+    {
+        canInteract = false;
+    }
+
+}
---
+++ Assets/Scripts/AirTube.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirTube : MonoBehaviour
+{
+
+    private List<GameObject> objectsOnConveyor = new List<GameObject>(); //Stores a list of gameobjects that are on the conveyor belt
+    private GameObject movingObject;                                     //Objects that will be stored in the list to be moved  
+    private Rigidbody movingObjectRB;
+
+    public bool isActive = true; 
+    public Vector3 direction;                                            //Direction where the object is pushed 
+    public float speed;                                                  //Speed for the movement of the object 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(isActive){
+            for(int i = 0; i <= objectsOnConveyor.Count -1; i++)
+            {
+                objectsOnConveyor[i].transform.position += direction * speed * Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Ticket Trigger"))
+        {
+            return;
+        }
+        objectsOnConveyor.Add(other.gameObject);
+        
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if(isActive){
+            movingObjectRB = other.gameObject.GetComponent<Rigidbody>();
+            movingObjectRB.useGravity = false;
+            movingObjectRB.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other) 
+    {
+        if(isActive){
+            movingObjectRB.constraints = RigidbodyConstraints.None;
+            movingObjectRB.useGravity = true; 
+        }
+        objectsOnConveyor.Remove(other.gameObject);
+    }
+}
