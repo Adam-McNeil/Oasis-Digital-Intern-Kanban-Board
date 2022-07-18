95a484d1cd3c40fb8f093a418024aaeb605fcb44
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-07-18 12:51:49 -0500

    WIP converts ticket data to json scale is not right

--- Assets/Scripts/TicketData.cs
+++ Assets/Scripts/TicketData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Mirror;
+using System.IO;
 
 public class TicketData : NetworkBehaviour
 {
@@ -17,8 +18,9 @@ public class TicketData : NetworkBehaviour
     public int materialData = 0;
 
     [SerializeField] private List<Material> materials = new List<Material>();
-
     [SerializeField] private TextMeshPro ticketHeaderText;
+
+    TicketSaveData ticketSaveData = new TicketSaveData();
 
     void Start()
     {
@@ -62,5 +64,82 @@ public class TicketData : NetworkBehaviour
         descriptionData = newDescription;
         assignedToData = newAssignTo;
         materialData = newMaterial;
+    }
+
+    string json;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Save();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ticketSaveData = JsonUtility.FromJson<TicketSaveData>(json);
+            Load();
+            ticketSaveData.print();
+        }
+    }
+
+    private void Save()
+    {
+        ticketSaveData.position = this.transform.position;                          
+        ticketSaveData.rotation = this.transform.rotation;
+        ticketSaveData.scale = this.transform.localScale;
+        ticketSaveData.constraints = this.GetComponent<Rigidbody>().constraints;
+        ticketSaveData.headerText = headerData;
+        ticketSaveData.descriptionText = descriptionData;
+        ticketSaveData.assignedToNumber = assignedToData;
+        ticketSaveData.materialNumber = materialData;
+        json = JsonUtility.ToJson(ticketSaveData);
+        Debug.Log(json);
+        Debug.Log(Application.dataPath);
+        File.AppendAllText(Application.dataPath + "/saveFile.json", json + "\n");
+    }
+
+    private void Load()
+    {
+        this.transform.position = ticketSaveData.position;
+        this.transform.rotation = ticketSaveData.rotation;
+        this.transform.localScale = ticketSaveData.scale;
+        this.GetComponent<Rigidbody>().constraints = ticketSaveData.constraints;
+
+        headerData = ticketSaveData.headerText;
+        ticketHeaderText.text = headerData;
+
+        descriptionData = ticketSaveData.descriptionText;
+        assignedToData = ticketSaveData.assignedToNumber;
+
+        materialData = ticketSaveData.materialNumber;
+        GetComponent<Renderer>().material = materials[materialData];
+
+    }
+
+    private class TicketSaveData
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+        public RigidbodyConstraints constraints;
+        public string headerText;
+        public string descriptionText;
+        public int assignedToNumber;
+        public int materialNumber;
+
+
+
+        public void print()
+        {
+            Debug.Log(position);
+            Debug.Log(rotation);
+            Debug.Log(scale);
+            Debug.Log(constraints);
+            Debug.Log(headerText);
+            Debug.Log(descriptionText);
+            Debug.Log(assignedToNumber);
+            Debug.Log(materialNumber);
+        }
+
     }
 }
