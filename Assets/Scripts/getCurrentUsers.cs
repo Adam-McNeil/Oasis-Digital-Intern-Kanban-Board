20f6f53911749219341f837dd61006854defac1b
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-07-26 14:03:09 -0500

    Removed Drop DIstance and added "Not Assigned" to drop down

--- Assets/Scripts/getCurrentUsers.cs
+++ Assets/Scripts/getCurrentUsers.cs
@@ -8,12 +8,13 @@ public class getCurrentUsers : MonoBehaviour
 
     public List<string> users = new List<string>();
     public List<string> usersOld = new List<string>();
+    public List<string> noBody = new List<string>();
     private FirebaseManager firebaseManager;
 
     private void Start()
     {
         firebaseManager = GameObject.Find("FirebaseManager").GetComponent<FirebaseManager>();
-        
+        noBody.Add("Not Assigned");
     }
 
     private void Update()
@@ -22,6 +23,7 @@ public class getCurrentUsers : MonoBehaviour
         if (users != usersOld)
         {
             this.GetComponent<TMP_Dropdown>().ClearOptions();
+            this.GetComponent<TMP_Dropdown>().AddOptions(noBody);
             this.GetComponent<TMP_Dropdown>().AddOptions(users);
             usersOld = users;
         }
