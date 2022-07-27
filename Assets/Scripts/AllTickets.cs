f7997fdc779780a7a44819813f8c363dccacebed
ManuelPaurevic <34079382+ManuelPaurevic@users.noreply.github.com>
2022-07-27 16:34:28 -0500

    Updated Script

--- Assets/Scripts/AllTickets.cs
+++ Assets/Scripts/AllTickets.cs
@@ -4,21 +4,70 @@ using UnityEngine;
 
 public class AllTickets : MonoBehaviour
 {
+    public bool nonSelcted = false;
+    public bool aden = false;
+    public bool adam = false;
+    public bool manny = false;
     public GameObject[] tickets;
+    int a = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(LateStart(.1f));
+        StartCoroutine(LateStart(.01f));
+    }
+
+    private void Update() {
+        
+        if(nonSelcted){
+            nonSelcted = false;
+            int a = 0;
+            GetAllTickets();
+            GetCertainTickets(0);
+        }
+
+        if(adam){
+            adam = false;
+            int a = 0;
+            GetAllTickets();
+            GetCertainTickets(1);
+        }
+        if(aden){
+            aden = false;
+            int a = 0;
+            GetAllTickets();
+            GetCertainTickets(2);
+        }
+        if(manny){
+            manny = false;
+            int a = 0;
+            GetAllTickets();
+            GetCertainTickets(3);
+        }
+
+
     }
  
     IEnumerator LateStart(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        //Your Function You Want to Call
-        tickets = GameObject.FindGameObjectsWithTag("Ticket");
-        Debug.Log("(AllTickets)This was Ran");
+        GetAllTickets();
     }
+
+    void GetAllTickets(){
+        tickets = null;
+        tickets = GameObject.FindGameObjectsWithTag("Ticket");
+        Debug.Log("All Tickets are found ");
+    }
+
+    void GetCertainTickets(int lookedFor){
+        for(int i = 0; i < tickets.Length; i++){
+            if(tickets[i].GetComponent<TicketData>().assignedToData == lookedFor){
+                    Debug.Log("Found: " + a++ + " | At Pos: " + i);
+            }
+        }
+    }
+
 
 
 }
