a9ea9edab71917da1664b22ceddd27d5cc455459
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-08-02 16:20:56 -0500

    fixed column effecting ticket movement

--- Assets/Scripts/BlueprintColliderCounter.cs
+++ Assets/Scripts/BlueprintColliderCounter.cs
@@ -5,13 +5,56 @@ using Mirror;
 
 public class BlueprintColliderCounter : NetworkBehaviour
 {
-    public int colliderCounter = 0;
+    public bool canPlaceColumn = false;
 
     [SerializeField] private List<Material> blueprintMaterials = new List<Material>();
+    PlayerController localPlayerController;
+    Vector3 overlapTestBlueprintScale = new Vector3(15, 20, 4);
+    float xOffset = -3.5f;
+    float yOffset = 10f;
+    Collider[] overlapedColliders;
 
     [SyncVar(hook = nameof(ChangeBlueprintMaterial))]
     private int blueprintMaterialIndex = -1;
 
+    private void Start()
+    {
+        localPlayerController = GameObject.FindGameObjectWithTag("Local Player").GetComponent<PlayerController>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (localPlayerController.isBuilding)
+        {
+            if (Physics.OverlapBox(this.transform.position + this.transform.right * xOffset + this.transform.up * yOffset, overlapTestBlueprintScale / 2, this.transform.rotation).Length == 0)
+            {
+                ChangeBlueprintMaterialCmd(1);
+                canPlaceColumn = true;
+            }
+            else
+            {
+                ChangeBlueprintMaterialCmd(0);
+                canPlaceColumn = false;
+            }
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    private void ChangeBlueprintMaterialCmd(int index)
+    {
+        blueprintMaterialIndex = index;
+    }
+
+    private void ChangeBlueprintMaterial(int oldMaterial, int newMaterial)
+    {
+        Renderer[] renderArray = GetComponentsInChildren<Renderer>();
+        Debug.Log("found this number of renders:" + renderArray.Length);
+        foreach (Renderer renderer in renderArray)
+        {
+            renderer.material = blueprintMaterials[newMaterial];
+        }
+    }
+/*
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("enterd collision");
@@ -30,24 +73,5 @@ public class BlueprintColliderCounter : NetworkBehaviour
         {
             ChangeBlueprintMaterialCmd(1);
         }
-    }
-
-    [Command(requiresAuthority = false)]
-    private void ChangeBlueprintMaterialCmd(int index)
-    {
-        //Debug.Log("called cahnge material command");
-        blueprintMaterialIndex = index;
-    }
-
-    private void ChangeBlueprintMaterial(int oldMaterial, int newMaterial)
-    {
-        //Debug.Log("called the change material hook");
-
-        Renderer[] renderArray = GetComponentsInChildren<Renderer>();
-        Debug.Log("found this number of renders:" + renderArray.Length);
-        foreach (Renderer renderer in renderArray)
-        {
-            renderer.material = blueprintMaterials[newMaterial];
-        }
-    }
+    }*/
 }
