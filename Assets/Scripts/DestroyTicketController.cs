68e26af16e3d57bad607c265c51067649bae9163
Adam-McNeil <adam.mcneil@oasisdigital.com>
2022-08-03 09:31:06 -0500

    updated trash can prefab and added sound

--- Assets/Scripts/DestroyTicketController.cs
+++ Assets/Scripts/DestroyTicketController.cs
@@ -5,13 +5,33 @@ using Mirror;
 
 public class DestroyTicketController : NetworkBehaviour
 {
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip destroyClip;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     [Server]
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ticket"))
         {
             Debug.Log("destroying ticekt");
+            PlaySoundRpc();
             NetworkServer.Destroy(collision.gameObject);
         }
+    }
+
+    [ClientRpc]
+    private void PlaySoundRpc()
+    {
+        PlaySound();
+    }
+
+    private void PlaySound()
+    {
+       audioSource.PlayOneShot(destroyClip);
     }
 }
